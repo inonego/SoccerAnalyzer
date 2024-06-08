c5de4f48461114725f0fa7699660f620fe8c0611
inonego <hmud0721@naver.com>
2024-06-08 15:25:42 +0900

    프레임 장면 변화 탐지 기능 추가

--- util.py
+++ util.py
@@ -14,7 +14,7 @@ def line_histogram_image(hist_img, position, color):
 
     return hist_img
 
-def get_histogram_image(hist, bin=256, height=256):        
+def get_histogram_image(hist, bin=256, height=256, color=(255, 255, 255)):        
     # 히스토그램을 이미지로 변환
     hist_img = np.zeros((height, bin, 3), dtype=np.uint8)
 
@@ -23,7 +23,7 @@ def get_histogram_image(hist, bin=256, height=256):
 
     # 히스토그램을 이미지에 그리기
     for x in range(bin):
-        cv2.line(hist_img, (x, height), (x, height - int(hist[x])), (255, 255, 255), 1)
+        cv2.line(hist_img, (x, height), (x, height - int(hist[x])), color, 1)
 
     return hist_img
 
